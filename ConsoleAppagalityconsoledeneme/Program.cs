ba1b615d876dd040d10e86d511f681533085c5ad
İbraihm Mert <i.m.bozdogan@hotmail.com>
2019-04-21 20:49:35 +0300

    deneme

--- ConsoleAppagalityconsoledeneme/Program.cs
+++ ConsoleAppagalityconsoledeneme/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleAppagalityconsoledeneme
             HtmlAgilityPack.HtmlDocument hdoc = hweb.Load("https://www.sozcu.com.tr/2017/otomotiv/boeing-777-ucak-degil-surucusuz-araba-1670919/");
             for (int i = 1; i < 40; i++)
             {
-                foreach (HtmlAgilityPack.HtmlNode item in hdoc.DocumentNode.SelectNodes("//*[@id=printable]/div[1]/div/div[1]/div[2][" + i + "]"))
+                foreach (HtmlAgilityPack.HtmlNode item in hdoc.DocumentNode.SelectNodes("//*[@id=printable"]/div[1]/div/div[1]/div[2][" + i + "]"))
                 {
                     
                     Console.WriteLine( item.InnerText.ToString);
